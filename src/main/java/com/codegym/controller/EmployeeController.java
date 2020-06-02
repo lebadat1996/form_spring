7a7cf03e38d182059e2d5e7a3b79613fadc2984c
badat <you@example.com>
2020-06-02 14:47:27 +0700

    bai tap spring

---
+++ src/main/java/com/codegym/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.codegym.controller;
+
+import com.codegym.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class EmployeeController {
+    @GetMapping()
+    public ModelAndView showIndex() {
+        ModelAndView modelAndView = new ModelAndView("/employee/index");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "showform", method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "employee/info";
+    }
+}
